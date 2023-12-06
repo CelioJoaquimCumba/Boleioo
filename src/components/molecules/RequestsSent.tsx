e5487293eba939e68ae650308b07931ddec7abe8
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-12-06 23:07:57 +0200

    refactor: add scroll hability

--- src/components/molecules/RequestsSent.tsx
+++ src/components/molecules/RequestsSent.tsx
@@ -2,7 +2,7 @@ import { RideCard } from "../atoms/RideCard"
 
 export const RequestsSent = () => {
     return(
-        <div className="space-y-2 self-stretch">
+        <div className="flex flex-col gap-2 self-stretch">
             {["1","2","3","4","5","6","7","8","9","10"].map(item => <RideCard key={item} driverName={"Olivia Doe"} startLocation={"Fomento, CABA"} endLocation={"Maputo, Mozambique"} match={10} startTime={"6:00 AM"} endTime={"7:00 AM"} avatar={"https://randomuser.me/api/portraits/women/37.jpg"}/>)}
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,16 +7,16 @@ export const Home = () => {
         <div className="flex self-stretch pb-4 flex-col items-end w-full h-screen overflow-hidden">
             <NavigationMenu/>
             {/* Map */}
-            <div className="flex w-full bg-green-500 h-full self-stretch">
+            <div className="flex w-full bg-green-500 self-stretch h-full">
                 Must have a map here
             </div>
-            <div className="flex w-full p-4 flex-col items-center gap-2 h-full overflow-y-auto">
-                <Tabs defaultValue="rides" className="flex flex-col items-center">
+            <div className="flex w-full p-4 flex-col items-center gap-2 h-full overflow-hidden">
+                <Tabs defaultValue="rides" className="flex flex-col items-center h-full overflow-auto">
                     <TabsList className="flex w-min">
                         <TabsTrigger value="rides">Available Rides</TabsTrigger>
                         <TabsTrigger value="requests">Requests Sent</TabsTrigger>
                     </TabsList>
-                    <TabsContent value="rides">
+                    <TabsContent value="rides" className="overflow-auto h-full">
                         <RequestsSent/>
                     </TabsContent>
                     <TabsContent value="requests">
